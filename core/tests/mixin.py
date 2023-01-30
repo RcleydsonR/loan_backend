7b9a6097e3d9c0c5698340e56820a1e3378cf67e
RcleydsonR <rafael.cleydson@gmail.com>
2023-01-29 22:01:35 -0300

    Add mixin on core to provide a logged user to tests

---
+++ core/tests/mixin.py
@@ -0,0 +1,48 @@
+from functools import partial
+
+from django.contrib.auth.models import User
+
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from model_bakery.recipe import Recipe
+
+
+class APITestMixin:
+
+    url = ''
+    username = 'Mocked user'
+    password = '12345678'
+    usuario_kwargs = {}
+
+    def get_header_credencial(self):
+        token = Token.objects.create(user=self.user)
+        return str(token.key)
+
+    def get_client(self):
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + self.get_header_credencial())
+        client.post = partial(client.post, format='json')
+        return client
+
+    def set_user_on_client(self, user):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.get_header_credencial(user))
+
+    def setUp(self):
+        self.user = Recipe(
+            User,
+            username=self.username, password=self.password, **self.usuario_kwargs
+        ).make()
+        self.user.id = self.user.id
+        self.client = self.get_client()
+
+    @classmethod
+    def setUpClass(cls):
+        if not isinstance(APITestMixin, cls) and cls.setUp is not APITestMixin.setUp:
+            setUp_original = cls.setUp
+
+            def setUpNew(self, *args, **kwargs):
+                APITestMixin.setUp(self)
+                setUp_original(self, *args, **kwargs)
+
+            cls.setUp = setUpNew
+        super().setUpClass()
